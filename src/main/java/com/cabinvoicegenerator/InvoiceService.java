f96498890e8e5d7193670681db053836fa11003c
Rushikesh Patil <rushipatil6632@gmail.com>
2022-03-29 21:05:57 +0530

    Added:Invoice service providing Invoice based on user id

--- src/main/java/com/cabinvoicegenerator/InvoiceService.java
+++ src/main/java/com/cabinvoicegenerator/InvoiceService.java
@@ -3,6 +3,11 @@ package com.cabinvoicegenerator;
 import com.bridgelabz.cabinvoicegenerator.model.InvoiceSummary;
 import com.bridgelabz.cabinvoicegenerator.model.Ride;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class InvoiceService {
     private static final double COST_PER_KM = 10;
     private static final double COST_PER_MIN = 1;
@@ -22,6 +27,7 @@ public class InvoiceService {
         double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
         return Math.max(totalFare, MIN_FARE);
     }
+
     public double calculateTotalFare(Ride[] rides) {
         double totalFare = 0.0;
         for (Ride ride : rides) {
@@ -30,6 +36,7 @@ public class InvoiceService {
         return totalFare;
 
     }
+
     public InvoiceSummary calculateFare(Ride[] rides) {
         double totalFare = 0.0;
         for (Ride ride : rides) {
@@ -38,4 +45,29 @@ public class InvoiceService {
         return new InvoiceSummary(rides.length, totalFare);
     }
 
+    public InvoiceSummary getInvoice(int userId) {
+        Map<Integer, Ride[]> map = new HashMap<>();
+        Ride[] rides1 = {new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        Ride[] rides2 = {new Ride(5.0, 10),
+                new Ride(1, 1)
+        };
+
+        Ride[] rides3 = {new Ride(8.0, 15),
+                new Ride(1, 10)
+        };
+        map.put(1, rides1);
+        map.put(2, rides2);
+        map.put(3, rides3);
+
+        for (Map.Entry<Integer, Ride[]> entry : map.entrySet()) {
+            if (userId == entry.getKey()) {
+                System.out.println(entry.getKey());
+                Ride[] ridesArray = entry.getValue();
+                return calculateFare(ridesArray);
+            }
+        }
+        return null;
+    }
 }
